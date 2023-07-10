e671dd8d74e9a8489f22852ae6c3cb0b0b8c396d
Jordan Hicks <jordanhicks@Jordans-MacBook-Pro.local>
2023-07-10 14:59:04 -0700

    First Commit

--- SpotifyCloneProject/YourSensitiveData.swift
+++ SpotifyCloneProject/YourSensitiveData.swift
@@ -1,4 +1,4 @@
 struct YourSensitiveData {
-  static let clientID = <YOUR_ID>
-  static let clientSecret = <YOUR_SECRET>
+  static let clientID = "8fecf068233947dca712e9961aaf5fa5"
+  static let clientSecret = "c91baf3c26f14ff38f7038e4ab714e92"
 }
